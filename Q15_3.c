e7c02a25769e0f16d0447a7c46411829a6c180b4
Kei.Narita <kei.narita1004@gmail.com>
2018-08-14 19:55:13 +0900

    getmaxmin関数のmaxの求め方が間違っていたので修正しました。

---
+++ Q15_3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+void getmaxmin(int *max,int *min,int data[10]);
+
+int main(void)
+{
+    int i;
+    int max,min;
+    int data[10];
+    for(i = 0;i < 10;i++){
+    printf("Input Nunber %d:",i);
+    scanf("%d",&data[i]);
+    if(data[i] == -1)break;
+    }
+    getmaxmin(&max,&min,data);
+    printf("max = %d min = %d\n",max,min);
+    return 0;
+}
+
+void getmaxmin(int *max,int *min,int *data)
+{
+    int i;
+    *min = data[0];
+    *max = data[0];
+    for(i = 0;i < 10;i++){
+        if(data[0] > data[i]){
+            *min = data[i];
+        }else if(data[0] < data[i]){
+            *max = data[i];
+        }
+    }
+        return;
+}
