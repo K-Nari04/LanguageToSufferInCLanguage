d315c20217aca33d9d8667e76ddc143adbae3a79
Kei.Narita <kei.narita1004@gmail.com>
2018-08-17 23:34:56 +0900

    回答を確認して入力用関数と表示用関数を整理

--- Q16.c
+++ Q16.c
@@ -6,37 +6,39 @@ typedef struct{
     int age; //年齢
 }profile;
 
-void profile_scan(profile data[]);
-void profile_print(profile data[]);
+void profile_scan(profile *data);
+void profile_print(profile data);
 
 int main(void)
 {
     profile data[10];
-    profile_scan(data);
-    profile_print(data);
+    int i;
+    for(i = 1;i < 4;i++){
+    profile_scan(&data[i]);
+    }
+    for(i = 1;i < 4;i++){
+    profile_print(data[i]);
+    }
     return 0;
 }
 
-void profile_scan(profile data[]){
-    int i;
-    for(i = 1;i < 4;i++){
-    printf("No:%d input your name :",i);
-    scanf("%s",data[i].name);
-    printf("No:%d input your sex :",i);
-    scanf("%s",data[i].sex);
-    printf("No:%d input your age :",i);
-    scanf("%d",&data[i].age);
-    }
+void profile_scan(profile *data){
+   
+    printf("input your name :");
+    scanf("%s",&data->name);
+    printf("input your sex :");
+    scanf("%s",&data->sex);
+    printf("input your age :");
+    scanf("%d",&data->age);
+    
     return;
 }
 
-void profile_print(profile data[])
+void profile_print(profile data)
 {
-    int i;
-    for(i = 1;i < 4;i++){
-    printf("name = %s\n",data[i].name);
-    printf("sex = %s\n",data[i].sex);
-    printf("age = %d\n",data[i].age);
-    }
+    printf("name = %s\n",data.name);
+    printf("sex = %s\n",data.sex);
+    printf("age = %d\n",data.age);
+
     return;
 }
