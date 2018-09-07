d43dcb097fb8113766b030d129270ae32e82e36e
Kei.Narita <kei.narita1004@gmail.com>
2018-09-07 12:18:22 +0900

    17章の解答を修正

--- Q17.c
+++ Q17.c
@@ -4,8 +4,8 @@ int main(void)
 {
     int a = 1,b = 2,c = 3,d = 4;
     int scoren = 0,scorem = 90,scoreg = 40,scoreh = 70;
-    char data1[] = "NO",data2[] = "Name",data3[] = "scoreave";
-    char name1[] = "nobi nobita",name2[] = "minamotoshizuka",name3[] = "goda takeshi",name4[] = "honekawa suneo";
+    char data1[] = "番号",data2[] = "名前",data3[] = "平均点";
+    char name1[] = "野比のび太",name2[] = "源静香",name3[] = "剛田武",name4[] = "骨川スネ夫";
     FILE *file;
     file = fopen("test.csv","w");
     fprintf(file,"%s,%s,%s\n",data1,data2,data3);
