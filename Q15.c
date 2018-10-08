9bc1670953aaefa002c22c5858e6fe9512325708
Kei.Narita <kei.narita1004@gmail.com>
2018-10-08 10:36:10 +0900

    １５章の解答の修正

--- Q15.c
+++ Q15.c
@@ -2,13 +2,11 @@
 #include <string.h>
 #include <stdlib.h>
 
-void getmax(int *max,int array[]);
-void getmin(int *max,int array[]);
+int getmax(const int array[]);
+int getmin(const int array[]);
 
 int main(void)
 {
-    int max;
-    int min;
     int i= 0;
     int array[10];
     char buffer[256];
@@ -18,27 +16,25 @@ int main(void)
        fgets(buffer,256,stdin);
        array[i]=  atoi(buffer);
     }
-    getmax(&max,array);
-    getmin(&min,array);
-    printf("max = %d : min = %d\n",max,min);
+    printf("max = %d : min = %d\n",getmax(array),getmin(array));
     return 0;
 }
 
-void getmax(int *max,int array[])
+int getmax(const int array[])
 {
     int i = 0;
-	*max = array[i];
+	int max = array[i];
     for(i = 0; i < 10;i++){
-    if(*max < array[i]){*max = array[i];}
+    if(max < array[i]){max = array[i];}
     } 
-    return;
+    return max;
 }
-void getmin(int *min,int array[])
+int getmin(const int array[])
 {
     int i = 0;
-	*min = array[i]; 
+	int min = array[i]; 
     for(i = 0; i < 10;i++){
-    if(*min > array[i]){*min = array[i];}  
+    if(min > array[i]){min = array[i];}  
     } 
-    return;
+    return min;
 }
